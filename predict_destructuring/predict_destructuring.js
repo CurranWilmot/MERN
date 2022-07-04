a35277d456aa235956f8bedbce4e591cc5fcf003
CurranWilmot <105678530+CurranWilmot@users.noreply.github.com>
2022-07-04 02:37:55 -0400

    Add files via upload
    playing around with syntax and destructuring in javascript

---
+++ predict_destructuring/predict_destructuring.js
@@ -0,0 +1,90 @@
+
+
+console.log("1.")
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+//prints 'Tesla'
+//prints 'Mercedes'
+
+
+
+console.log("2.")
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+//****************************
+//commented out so code below will run
+// console.log(name);
+// console.log(otherName);
+//****************************
+
+//error, "name" isn't defined
+//(you can see the line through it)
+
+
+
+console.log("3.")
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+//prints '12345'
+//prints undefined
+
+
+
+
+console.log("4.")
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+
+//prints false
+//prints true
+
+
+
+
+console.log("5.")
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+//prints 'value'
+//prints [1, 5, 1, 8, 3, 3]
+//prints 1
+//prints 5
