b71c7131cc51e554732762ad5ad12c2908962a0d
CurranWilmot <105678530+CurranWilmot@users.noreply.github.com>
2022-07-07 03:26:57 -0400

    Add files via upload
    playing around with jsx

---
+++ first_jsx/first_jsx/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='hello_dojo'>
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <li>Learn React</li>
+      <li>Learn React</li>
+      <li>Learn React</li>
+      <li>Learn React</li>
+    </div>
+  );
+}
+
+export default App;
